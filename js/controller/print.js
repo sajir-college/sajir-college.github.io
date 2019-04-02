c18f2caa0af67bfdbf57cfb04d02a25700e43b4a
mkoubaa <mkoubaa.sa@gmail.com>
2019-04-02 21:39:29 +0300

    update print.json

--- js/controller/print.js
+++ js/controller/print.js
@@ -4,7 +4,7 @@ todoApp.controller('PrintCtrl', function($scope) {
         var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
         popupWinindow.document.open();
         popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head>');
-        popupWinindow.document.write('<body onload="window.print()  onfocus="window.close()">');
+        popupWinindow.document.write('<body onload="window.print()">');
         popupWinindow.document.write('<img src="logo-su.png" alt="Logo" class="center">');
         popupWinindow.document.write('<header><h1>كلية العلوم والآداب بساجر</h1></header>');
         popupWinindow.document.write(innerContents);
