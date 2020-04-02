84709819e9549b770fd74a67725c118a6df26d2c
mkoubaa <mkoubaa.sa@gmail.com>
2020-04-02 20:50:12 +0300

    new files for 412

---
+++ 392/Surv/js/controller/exams.js
@@ -0,0 +1,39 @@
+todoApp.controller("ExamsCtrl", function ($scope, ExamsFactory) {
+    $scope.todo = ExamsFactory.getExams().then(function(){
+              $scope.todo=ExamsFactory.exams;
+              console.log($scope.todo);
+
+
+    }, function(msg){
+      alert(msg);
+    });//model;
+
+    $scope.IsEmpty=true;
+    $scope.IsNotFound=false;
+    $scope.result=[];
+    /*$scope.addNewItem = function (actionText) {
+        //$scope.todo.items.push({ action: actionText, done: false });
+      }*/
+
+    /*  $scope.change= function(){
+        console.log("hello");
+        $scope.IsNotFound=false;
+      }*/
+
+      $scope.selectExams = function (id) {
+                console.log($scope.todo);
+                $scope.result=[];
+                $scope.IsEmpty=true;
+                $scope.IsNotFound=true;
+
+                angular.forEach($scope.todo, function(value, key){
+
+                      if(value.ID===id){
+                        console.log(value);
+
+                            $scope.result.push(value);
+                            $scope.IsEmpty=false;
+                          $scope.IsNotFound=false; }
+                });
+      }
+});
