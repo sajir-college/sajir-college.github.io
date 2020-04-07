68d05f90945d126bd975d4474ae0d1f065e85656
mkoubaa <mkoubaa.sa@gmail.com>
2020-04-07 16:35:24 +0300

    ajout folder 69

---
+++ 69/js/service/factory.js
@@ -0,0 +1,23 @@
+todoApp.factory('ExamsFactory', function($http, $q){
+  var factory={
+                        exams: false,
+                        getExams: function(){
+                          var deferred = $q.defer();
+                          if(factory.exams!=false){
+                            deferred.resolve(factory.exams);
+                          }
+                          else{
+                                  $http({
+                                               method: 'GET',
+                                               url: 'student411.json'
+                                              }).then(function successCallback(response){
+                                               factory.exams = response.data;
+                                               deferred.resolve(factory.exams);
+                                              }, function errorCallback(response){
+                                               alert(response);
+                                              });
+                                    }
+                        return deferred.promise;},
+                      }
+                      return factory;
+                      });
